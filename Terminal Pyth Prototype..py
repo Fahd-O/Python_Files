c06e7f4415a5ed0bec3993e79b900c226a6058fe
Abiola Fahad Ogunnaike <realabiolafahad@gmail.com>
2024-10-20 16:45:27 +0100

    first commit

---
+++ Terminal Pyth Prototype..py
@@ -0,0 +1,25 @@
+Last login: Mon Jul 20 07:22:28 on ttys002
+Fahds-pc:~ fahadabiola$ python
+Python 2.7.10 (default, Oct 23 2015, 19:19:21) 
+[GCC 4.2.1 Compatible Apple LLVM 7.0.0 (clang-700.0.59.5)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> x=int(input('Enter 1st number here')
+>>> y=int(input('Enter 2nd number here')
+>>> z=int(input('Enter 3rd number here')
+>>> 
+>>> 
+>>> print(max(x,y,z))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ while loop.py
@@ -0,0 +1,5 @@
+i=1+1
+while True:
+	i=2
+	print('Finish')
+	
